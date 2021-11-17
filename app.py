1d98f079708a3e97773b58598a81b8f55e625c7b
Commeilfaut <stanina238@rambler.ru>
2021-11-17 23:55:21 +0300

    next

--- app.py
+++ app.py
@@ -4,8 +4,19 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/about')
+def about():
+    return 'About page'
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return 'User page:' + name + " - " + str(id)
 
 
 if __name__ == '__main__':
